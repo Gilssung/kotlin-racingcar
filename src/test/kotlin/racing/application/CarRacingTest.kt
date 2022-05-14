3dbe71fc0f2c091dafbdd466a6ce6be3a1d1ce6a
qyu.st <qyu.st@kakaocorp.com>
2022-05-14 13:13:41 +0900

    refactor: test package 구성 변경 및 Car model test 추가

--- src/test/kotlin/racing/application/CarRacingTest.kt
+++ src/test/kotlin/racing/application/CarRacingTest.kt
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import racing.model.Car
+<<<<<<< HEAD
 import racing.model.Round
+=======
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
 
 internal class CarRacingTest {
 
     @Test
     fun `get random value between 0 - 9`() {
         repeat(100) {
+<<<<<<< HEAD
             //given
             val numberOfRandomInt = it + 1
             val randomValueFrom = 0
@@ -26,11 +30,17 @@ internal class CarRacingTest {
             assertThat(result)
                 .isGreaterThanOrEqualTo(randomValueFrom)
                 .isLessThanOrEqualTo(randomValueTo)
+=======
+            assertThat(CarRacing.getRandom())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9)
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
         }
     }
 
     @Test
     fun `first round do initialization test`() {
+<<<<<<< HEAD
         //given
         val firstRoundNo = 0
         val numberOfCars = 3
@@ -44,6 +54,16 @@ internal class CarRacingTest {
         //then
         assertThat(result[firstRoundNo])
             .extracting("cars")
+=======
+        val carRacing = CarRacing(3, 3)
+        assertThat(carRacing.eachRoundMap)
+            .isEmpty()
+
+        carRacing.round(1)
+        val expected = List(3) { Car() }
+
+        assertThat(carRacing.eachRoundMap[1])
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
             .isEqualTo(expected)
     }
 
@@ -54,6 +74,7 @@ internal class CarRacingTest {
         "23, 15",
         "1231, 1"
     )
+<<<<<<< HEAD
     fun `start method create round result`(numberOfCars: Int, tries: Int) {
         //given
         val expectedKeys = Array(tries) { it }
@@ -64,6 +85,15 @@ internal class CarRacingTest {
 
         //then
         assertThat(racingResult)
+=======
+    fun `start method create each round result`(numberOfCars: Int, tries: Int) {
+        val carRacing = CarRacing(numberOfCars, tries)
+        carRacing.start()
+
+        val expectedKeys = Array(tries) { it }
+        val expectedValue = List(numberOfCars) { Car() }
+        assertThat(carRacing.eachRoundMap)
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
             .containsKeys(*expectedKeys)
             .containsEntry(0, expectedValue)
     }
--- src/test/kotlin/racing/model/CarTest.kt
+++ src/test/kotlin/racing/model/CarTest.kt
@@ -9,6 +9,7 @@ internal class CarTest {
 
     @Test
     fun `create default car`() {
+<<<<<<< HEAD
         //given
         val expected = "-"
 
@@ -16,11 +17,17 @@ internal class CarTest {
         val result = Car().position
 
         //then
+=======
+        val result = Car().position
+        val expected = "-"
+
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
         assertThat(result).isEqualTo(expected)
     }
 
     @Test
     fun `toString test`() {
+<<<<<<< HEAD
         //given
         val expected = "-"
 
@@ -28,12 +35,18 @@ internal class CarTest {
         val result = Car().toString()
 
         //then
+=======
+        val result = Car().toString()
+        val expected = "-"
+
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
         assertThat(result).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [1,2,3,4,5])
     fun `go test`(loop: Int) {
+<<<<<<< HEAD
         //given
         val expected = "-".repeat(loop)
 
@@ -69,4 +82,14 @@ internal class CarTest {
         } else {
             1
         }
+=======
+
+        val result = List(loop - 1) { it }
+            .fold(Car()) { acc, _ -> acc.go()}
+            .toString()
+        val expected = "-".repeat(loop)
+
+        assertThat(result).isEqualTo(expected)
+    }
+>>>>>>> ef70116 (refactor: test package 구성 변경 및 Car model test 추가)
 }
