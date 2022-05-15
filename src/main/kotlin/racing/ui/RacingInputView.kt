e42f0ffc8a7e441ce8e7b471c6eee48a462c8a5b
qyu.st <qyu.st@kakaocorp.com>
2022-05-15 18:10:50 +0900

    refactor: 상태가 없는 RacingInputView object로 선언
    * https://github.com/next-step/kotlin-racingcar/pull/536#discussion_r873121160

--- src/main/kotlin/racing/ui/RacingInputView.kt
+++ src/main/kotlin/racing/ui/RacingInputView.kt
@@ -1,6 +1,7 @@
 package racing.ui
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 object RacingInputView {
 
     fun getNumberOfCars(): Int {
@@ -13,17 +14,25 @@ object RacingInputView {
         return readln().toInt()
 =======
 class RacingInputView {
+=======
+object RacingInputView {
+>>>>>>> 8173282 (refactor: 상태가 없는 RacingInputView object로 선언)
 
-    companion object {
-        fun getNumberOfCars(): Int {
-            println("자동차 대수는 몇 대인가요?")
-            return readln().toInt()
-        }
+    fun getNumberOfCars(): Int {
+        println("자동차 대수는 몇 대인가요?")
+        return readln().toInt()
+    }
 
+<<<<<<< HEAD
         fun getTries(): Int {
             println("시도할 횟수는 몇 회인가요?")
             return readln().toInt()
         }
 >>>>>>> 1a814e9 (refactor: package 구성 변경)
+=======
+    fun getTries(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readln().toInt()
+>>>>>>> 8173282 (refactor: 상태가 없는 RacingInputView object로 선언)
     }
 }
